1a7c98ce12f76fee76fe38fced681e5a11585a53
GustavoTZ14 <gterrin155@gmail.com>
2023-06-08 17:52:49 -0300

    gustavotz

---
+++ javascript/exF_1.js
@@ -0,0 +1,21 @@
+function falaOi() {
+  console.log('Oie');
+}
+falaOi();
+
+const souUmDado = function() {
+  console.log('Sou um dado.');
+}
+souUmDado();
+
+const arrowFunction = () => {
+  console.log('Sou uma arrow function.');
+}
+arrowFunction();
+
+const obj = {
+  falar() {
+    console.log('Estou Falando...');
+  }
+};
+obj.falar();
